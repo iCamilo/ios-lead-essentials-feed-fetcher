963075a372803464ad87ae33937f80c8aa7bb42d
Iván Fuertes <ivancamilofuerte>
2020-07-03 22:06:12 -0500

    Decouple `FeedRefreshController` from `FeedPresenter` by using a closure instead

--- FeedFetcher/FeedFetcheriOS/Feed UI/Composers/FeedUIComposer.swift
+++ FeedFetcher/FeedFetcheriOS/Feed UI/Composers/FeedUIComposer.swift
@@ -10,7 +10,7 @@ public final class FeedUIComposer {
     
     public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
         let feedPresenter = FeedPresenter(feedLoader: feedLoader)
-        let refreshController = FeedRefreshViewController(presenter: feedPresenter)
+        let refreshController = FeedRefreshViewController(loadFeed: feedPresenter.loadFeed)
         let feedController = FeedViewController(refreshController: refreshController)
         
         feedPresenter.feedLoadingView = WeakRefVirtualProxy(refreshController)
