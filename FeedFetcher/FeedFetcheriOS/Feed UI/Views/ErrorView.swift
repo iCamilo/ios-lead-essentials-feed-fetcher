237ada09dcaaaf70089a43bfb18b284fd7663979
Iván Fuertes <ivancamilofuerte>
2020-07-14 15:24:56 -0500

    Add `ErrorView` swift file and required image asset

---
+++ FeedFetcher/FeedFetcheriOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,55 @@
+//  Created by Ivan Fuertes on 14/07/20.
+//  Copyright © 2020 Ivan Fuertes. All rights reserved.
+
+import UIKit
+
+public final class ErrorView: UIView {
+    @IBOutlet private var container: UIView!
+    @IBOutlet private var label: UILabel!
+    
+    public var message: String? {
+        get { return isVisible ? label.text : nil }
+    }
+    
+    private var isVisible: Bool {
+        return alpha > 0
+    }
+    
+    public override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        addRounderBorderColor()
+        label.text = nil
+        alpha = 0
+    }
+    
+    func show(message: String) {
+        self.label.text = message
+        
+        UIView.animate(withDuration: 0.25) {
+            self.alpha = 1
+        }
+    }
+    
+    @IBAction func hideMessage() {
+        UIView.animate(
+            withDuration: 0.25,
+            animations: { self.alpha = 0 },
+            completion: { completed in
+                if completed { self.label.text = nil }
+        })
+    }
+    
+    private func addRounderBorderColor() {
+        container.layer.cornerRadius = 5
+        container.layer.borderWidth = 2
+        container.layer.borderColor = UIColor.cayenne.cgColor
+    }
+}
+
+private extension UIColor {
+    static var cayenne: UIColor {
+        UIColor(red: 148/255, green: 17/255, blue: 0, alpha: 1.0)
+    }
+}
+
