c84e624dde675cfef35239dfd9ff3212d7a92439
Iván Fuertes <ivancamilofuerte>
2020-07-16 20:15:35 -0500

    Move `RemoteFeedImageLoader` to production

---
+++ FeedFetcher/FeedFetcher/FeedAPI/RemoteFeedImageLoader.swift
@@ -0,0 +1,72 @@
+//  Created by Ivan Fuertes on 16/07/20.
+//  Copyright © 2020 Ivan Fuertes. All rights reserved.
+
+import Foundation
+
+public final class RemoteFeedImageLoader: FeedImageDataLoader {
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+    
+    private let httpClient: HttpClient
+    
+    public init(httpClient: HttpClient) {
+        self.httpClient = httpClient
+    }
+        
+    @discardableResult
+    public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataTask {
+        let task = RemoteFeedImageTask(completion: completion)
+        task.wrappedHttpTask = httpClient.get(from: url) { [weak self] httpResult in
+            guard let self = self else {
+                return
+            }
+            
+            let result = self.handle(httpResult: httpResult)
+            task.complete(with: result)
+        }
+        
+        return task
+    }
+
+    private func handle(httpResult: HttpClient.Result) -> FeedImageDataLoader.Result {
+        return httpResult
+            .mapError { _ in Error.connectivity}
+            .flatMap ({ (response, data) in
+                let isValidResponse = self.isValid((response, data))
+                
+                return isValidResponse ? .success(data) : .failure(Error.invalidData)
+            })
+    }
+    
+    private func isValid(_ result: (response: HTTPURLResponse, data: Data)) -> Bool {
+        let OK_200 = 200
+        
+        return result.response.statusCode == OK_200 && !result.data.isEmpty
+    }
+}
+
+// MARK:- RemoteFeedImageTask
+
+private class RemoteFeedImageTask: FeedImageDataTask {
+    private var completion: ((RemoteFeedImageLoader.Result) -> Void)?
+    var wrappedHttpTask: HttpClientTask?
+            
+    init(completion: @escaping (RemoteFeedImageLoader.Result) -> Void) {
+        self.completion = completion
+    }
+                            
+    func cancel() {
+        wrappedHttpTask?.cancel()
+        invalidateCompletion()
+    }
+    
+    func complete(with result: RemoteFeedImageLoader.Result) {
+        completion?(result)
+    }
+    
+    private func invalidateCompletion() {
+        completion = nil
+    }
+}
--- FeedFetcher/FeedFetcherTests/FeedAPI/RemoteFeedImageLoaderTests.swift
+++ FeedFetcher/FeedFetcherTests/FeedAPI/RemoteFeedImageLoaderTests.swift
@@ -5,73 +5,6 @@ import Foundation
 import XCTest
 import FeedFetcher
 
-final class RemoteFeedImageLoader {
-    typealias Result = Swift.Result<Data, Swift.Error>
-    
-    enum Error: Swift.Error {
-        case connectivity
-        case invalidData
-    }
-    
-    private let httpClient: HttpClient
-    
-    init(httpClient: HttpClient) {
-        self.httpClient = httpClient
-    }
-        
-    @discardableResult
-    func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataTask {
-        let task = RemoteFeedImageTask(completion: completion)
-        task.wrappedHttpTask = httpClient.get(from: url) { [weak self] httpResult in
-            guard let self = self else {
-                return
-            }
-            
-            let result = self.handle(httpResult: httpResult)
-            task.complete(with: result)
-        }
-        
-        return task
-    }
-
-    private func handle(httpResult: HttpClient.Result) -> RemoteFeedImageLoader.Result {
-        return httpResult
-            .mapError { _ in Error.connectivity}
-            .flatMap ({ (response, data) in
-                let isValidResponse = self.isValid((response, data))
-                return isValidResponse ? .success(data) : .failure(Error.invalidData)
-            })
-    }
-    
-    private func isValid(_ result: (response: HTTPURLResponse, data: Data)) -> Bool {
-        let OK_200 = 200
-        
-        return result.response.statusCode == OK_200 && !result.data.isEmpty
-    }
-}
-
-private class RemoteFeedImageTask: FeedImageDataTask {
-    private var completion: ((RemoteFeedImageLoader.Result) -> Void)?
-    var wrappedHttpTask: HttpClientTask?
-            
-    init(completion: @escaping (RemoteFeedImageLoader.Result) -> Void) {
-        self.completion = completion
-    }
-                            
-    func cancel() {
-        wrappedHttpTask?.cancel()
-        invalidateCompletion()
-    }
-    
-    func complete(with result: RemoteFeedImageLoader.Result) {
-        completion?(result)
-    }
-    
-    private func invalidateCompletion() {
-        completion = nil
-    }
-}
-
 class RemoteFeedImageLoaderTests: XCTestCase {
         
     func test_init_doesNotRequestData() {
@@ -185,7 +118,7 @@ class RemoteFeedImageLoaderTests: XCTestCase {
         return(sut, client)
     }
     
-    private func expect(_ sut: RemoteFeedImageLoader, toCompleteWith expected: RemoteFeedImageLoader.Result, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
+    private func expect(_ sut: RemoteFeedImageLoader, toCompleteWith expected: FeedImageDataLoader.Result, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
         let exp = expectation(description: "Waiting for image load to complete")
         
         sut.loadImageData(from: anyURL()) { result in
