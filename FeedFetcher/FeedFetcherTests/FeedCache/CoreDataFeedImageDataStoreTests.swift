595f5a762af9eba4d54dcff903321d73f4df508d
Iván Fuertes <ivancamilofuerte>
2020-07-21 15:50:01 -0500

    Extract insert setup and asserts to a helper method

--- FeedFetcher/FeedFetcherTests/FeedCache/CoreDataFeedImageDataStoreTests.swift
+++ FeedFetcher/FeedFetcherTests/FeedCache/CoreDataFeedImageDataStoreTests.swift
@@ -15,31 +15,12 @@ class CoreDataFeedImageDataStoreTests: XCTestCase {
     
     func test_retrieveImageData_completesWithNotFoundForNonStoredURL() {
         let sut = makeSUT()
-        let storeImage = localImage(url: anyURL())
-        let storeImageURL = storeImage.url
-        let noStoreImageURL = URL(string: "http://no-storedImageURL.png")!
-                
-        let insertExp = expectation(description: "Waiting insert image data to complete")
-        sut.insert([storeImage], timestamp: Date()) { insertFeedResult in
-            switch insertFeedResult {
-            case let .failure(insertFeedImageError):
-                XCTFail("Failed to insert feed image with error \(insertFeedImageError)")
-                insertExp.fulfill()
-                
-            case .success:
-                sut.insertImageData(anyData(), for: storeImageURL) { insertDataResult in
-                    if case let .failure(error) = insertDataResult {
-                        XCTFail("Failed to insert image data with error \(error)")
-                    }
-                    
-                    insertExp.fulfill()
-                }
-                
-            }
-        }
-        wait(for: [insertExp], timeout: 1.0)
+        let url = anyURL()
+        let nonExistentURL = URL(string: "http://no-storedImageURL.png")!
+                                
+        insert(anyData(), for: url, into: sut)
                                         
-        expect(sut, toCompleteRetrievalWith: .success(.none), for: noStoreImageURL)
+        expect(sut, toCompleteRetrievalWith: .success(.none), for: nonExistentURL)
     }
     
 }
@@ -71,6 +52,34 @@ private extension CoreDataFeedImageDataStoreTests {
                 XCTFail("Expected to complete with not found data for url that is not stored, but got \(result)", file: file, line: line)
             }
             exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    func insert(_ data: Data, for url: URL, into sut: CoreDataFeedStore, file: StaticString = #file, line: UInt = #line) {
+        let image = localImage(url: url)
+        let exp = expectation(description: "Waiting insert image data to complete")
+        
+        sut.insert([image], timestamp: Date()) { insertFeedResult in
+            switch insertFeedResult {
+            
+            case let .failure(insertFeedImageError):
+                XCTFail("Failed to insert feed image with error \(insertFeedImageError)", file: file, line: line)
+                exp.fulfill()
+                
+            case .success:
+                sut.insertImageData(data, for: url) { insertDataResult in
+                    switch insertDataResult {
+                    case .success:
+                        break
+                    case let .failure(error):
+                        XCTFail("Failed to insert image data with error \(error)", file: file, line: line)
+                    }
+                    
+                    exp.fulfill()
+                }
+            }
         }
         
         wait(for: [exp], timeout: 1.0)
