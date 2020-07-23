246b1e11410382396682391bcb91e8f2f8a4fd96
Iván Fuertes <ivancamilofuerte>
2020-07-23 14:36:49 -0500

    Create empty FeedFetch project

---
+++ FeedFetchApp/FeedFetchApp/ViewController.swift
@@ -0,0 +1,15 @@
+//  Created by Ivan Fuertes on 23/07/20.
+//  Copyright © 2020 Ivan Fuertes. All rights reserved.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
