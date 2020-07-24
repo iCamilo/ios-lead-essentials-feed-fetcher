59989452123c58bb3045b0f393a9559a15d09bb1
Iván Fuertes <ivancamilofuerte>
2020-07-23 19:27:40 -0500

    Compose `FeedViewController` with `remoteFeedLoaderWithLocalFallback` and `localImageDataLoaderWithRemoteFallback`

--- FeedFetchApp/FeedFetchApp/SceneDelegate.swift
+++ FeedFetchApp/FeedFetchApp/SceneDelegate.swift
@@ -4,6 +4,7 @@
 import UIKit
 import FeedFetcher
 import FeedFetcheriOS
+import CoreData
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -11,18 +12,55 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
+                                
+        let (remoteFeedLoaderWithLocalFallback, localImageDataLoaderWithRemoteFallback) = composeFeedLoadersWithFallback()
+        let feedViewController = FeedUIComposer.feedComposedWith(
+            feedLoader: remoteFeedLoaderWithLocalFallback,
+            imageLoader: localImageDataLoaderWithRemoteFallback)
         
-        let session = URLSession(configuration: .ephemeral)
-        let httpClient = URLSessionHttpClient(session: session)
-        let url = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
-        
-        let feedLoader = RemoteFeedLoader(from: url, httpClient: httpClient)
-        let imageLoader = RemoteFeedImageLoader(httpClient: httpClient)
-        
-        let feedViewController = FeedUIComposer.feedComposedWith(feedLoader: feedLoader, imageLoader: imageLoader)
-        
-        window?.rootViewController = feedViewController
+        window?.rootViewController = feedViewController                  
     }
 
+    
+    
+}
+
+private extension SceneDelegate {
+                
+    func composeFeedLoadersWithFallback() -> (feed: FeedLoaderWithFallbackComposite, image: FeedImageDataLoaderWithFallbackComposite) {
+        let (remoteFeedLoader, remoteImageDataLoader) = makeRemoteFeedLoader()
+        let (localFeedLoader, localImageDataLoader) = makeLocalFeedLoader()
+        
+        let feedComposite = FeedLoaderWithFallbackComposite(primaryFeedLoader: remoteFeedLoader, fallbackFeedLoader: localFeedLoader)
+        let imageComposite = FeedImageDataLoaderWithFallbackComposite(primaryLoader: localImageDataLoader, fallbackLoader: remoteImageDataLoader)
+        
+        return (feedComposite, imageComposite)
+    }
+    
+    func makeLocalFeedLoader() -> (feed: LocalFeedLoader, image: LocalFeedImageDataLoader) {
+        let localStoreURL = NSPersistentContainer.defaultDirectoryURL().appendingPathComponent("feed-store.sqlite")
+        let feedStore = try! CoreDataFeedStore(storeURL: localStoreURL)
+        
+        let localFeedLoader = LocalFeedLoader(store: feedStore, currentDate: Date.init)
+        let localImageDataLoader = LocalFeedImageDataLoader(store: feedStore)
+        
+        return (localFeedLoader, localImageDataLoader)
+    }
+            
+    func makeRemoteFeedLoader() -> (feed: RemoteFeedLoader, image: RemoteFeedImageLoader) {
+        let session = URLSession(configuration: .ephemeral)
+        let httpClient = URLSessionHttpClient(session: session)
+        
+        let remoteFeedLoader = RemoteFeedLoader(from: remoteFeedLoaderURL, httpClient: httpClient)
+        let remoteImageDataLoader = RemoteFeedImageLoader(httpClient: httpClient)
+        
+        return (remoteFeedLoader, remoteImageDataLoader)
+    }
+    
+    var remoteFeedLoaderURL: URL {
+        URL(string:
+            "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
+    }
+    
 }
 
