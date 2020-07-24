9d96501244fb1bd50da1eae2173b7eb6f3d51074
Iván Fuertes <ivancamilofuerte>
2020-07-24 12:16:00 -0500

    `FeedLoaderCacheDecorator.load` delivers decoratee load result (success/failure)
    Validate that the decorator uses and routes the decoratee completion results

---
+++ FeedFetchApp/FeedFetchAppTests/FeedLoaderCacheDecoratorTests.swift
@@ -0,0 +1,85 @@
+//  Created by Ivan Fuertes on 24/07/20.
+//  Copyright © 2020 Ivan Fuertes. All rights reserved.
+
+import Foundation
+import XCTest
+import FeedFetcher
+
+final class FeedLoaderCacheDecorator: FeedLoader {
+        
+    private let decoratee: FeedLoader
+    
+    init(decoratee: FeedLoader) {
+        self.decoratee = decoratee
+    }
+    
+    func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        decoratee.load(completion: completion)
+    }
+        
+}
+
+final class FeedLoaderCacheDecoratorTests: XCTestCase {
+    
+    func test_load_deliversFeedOnLoaderSucess() {
+        let feed = uniqueFeed()
+        let loader = LoaderStub(result: .success(feed))
+        let sut = FeedLoaderCacheDecorator(decoratee: loader)
+                        
+        expect(sut, toCompleteWith: .success(feed))
+    }
+    
+    func test_load_deliversErrorOnLoaderFailure() {
+        let loaderError = anyError
+        let loader = LoaderStub(result: .failure(loaderError))
+        let sut = FeedLoaderCacheDecorator(decoratee: loader)
+                        
+        expect(sut, toCompleteWith: .failure(loaderError))
+    }
+    
+    
+}
+
+// MARK:- Helpers
+
+private extension FeedLoaderCacheDecoratorTests {
+        
+    func expect(_ sut: FeedLoaderCacheDecorator, toCompleteWith expected: FeedLoader.Result, file: StaticString = #file, line: UInt = #line) {
+        let exp = expectation(description: "Waiting for load to complete")
+        
+        sut.load { result in
+            switch (result, expected) {
+            case let (.success(resultFeed), .success(expectedFeed)):
+                XCTAssertEqual(resultFeed, expectedFeed, "Expected load to complete with feed \(expectedFeed) but got \(resultFeed)", file: file, line: line)
+            case let (.failure(resultError as NSError), .failure(expectedError as NSError)):
+                XCTAssertEqual(resultError, expectedError, "Expected load to fail with error \(expectedError) but got \(resultError)", file: file, line: line)
+            default:
+                XCTFail("Expected load to complete with result \(expected) but got \(result)")
+            }
+                                    
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    func uniqueFeed() -> [FeedImage] {
+        return [FeedImage(id: UUID(), url: anyURL, description: "anyDescription", location:"anyLocation")]
+    }
+    
+}
+
+// MARK:- LoaderStub
+
+private class LoaderStub: FeedLoader {
+    private let result: FeedLoader.Result
+    
+    init(result: FeedLoader.Result) {
+        self.result = result
+    }
+    
+    func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        completion(result)
+    }
+    
+}
