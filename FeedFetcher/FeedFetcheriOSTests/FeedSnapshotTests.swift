7de5da601708639b6ae981676346340aa11a56b5
Iván Fuertes <ivancamilofuerte>
2020-07-28 19:24:08 -0500

    Record empty feed snapshot

---
+++ FeedFetcher/FeedFetcheriOSTests/FeedSnapshotTests.swift
@@ -0,0 +1,71 @@
+//  Created by Ivan Fuertes on 28/07/20.
+//  Copyright © 2020 Ivan Fuertes. All rights reserved.
+
+import Foundation
+import XCTest
+import FeedFetcheriOS
+
+final class FeedSnapshotTests: XCTestCase {
+    
+    func test_emptyFeed() {
+        let sut = makeSUT()
+        
+        sut.display(emptyFeed())
+        
+        record(snapshot: sut.snapshot(), name: "EMPTY_FEED")
+    }
+}
+
+// MARK:- Helpers
+
+private extension FeedSnapshotTests {
+    
+    func makeSUT() -> FeedViewController {
+        let bundle = Bundle(for: FeedViewController.self)
+        let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
+        let controller = storyboard.instantiateInitialViewController() as! FeedViewController
+        controller.loadViewIfNeeded()
+        
+        return controller
+    }
+    
+    func record(snapshot: UIImage, name: String, file: StaticString = #file, line: UInt = #line) {
+        guard let snapshotData = snapshot.pngData() else {
+            XCTFail("Failed to generate png data representation from snapshot", file: file, line: line)
+            return
+        }
+        
+        let snapshotURL = URL(fileURLWithPath: String(describing: file))
+            .deletingLastPathComponent()
+            .appendingPathComponent("snapshots")
+            .appendingPathComponent(name)
+            .appendingPathExtension("png")
+        
+        do {
+            try FileManager.default.createDirectory(at: snapshotURL.deletingLastPathComponent(), withIntermediateDirectories: true)
+            
+            try snapshotData.write(to: snapshotURL)
+        } catch {
+            XCTFail("Failed to record snapshot with error: \(error)", file: file, line: line)
+        }
+        
+    }
+    
+    func emptyFeed() -> [FeedImageCellController] {
+        return []
+    }
+    
+}
+
+// MARK:- Snapshot
+
+extension UIViewController {
+    
+    func snapshot() -> UIImage {
+        let renderer = UIGraphicsImageRenderer(bounds: view.bounds)
+        return renderer.image { action in
+            view.layer.render(in: action.cgContext)
+        }
+    }
+    
+}
